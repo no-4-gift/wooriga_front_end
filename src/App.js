73df8e5fe21398dcacba516c5bf4a3b97b24dc22
DESKTOP-SAJN8LR\PC <tmdwns1101@naver.com>
2019-09-09 13:32:46 +0900

    commit test

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import React from 'react';
-import {BrowserRouter, Route} from 'react-router-dom';
-
+import React from "react";
+import { BrowserRouter, Route } from "react-router-dom";
+import Test from "./containers/Test";
 function App() {
   return (
     <BrowserRouter>
       {/* <Route path="/counter" component={src}/> */}
+      <Route path="/" component={Test} />
     </BrowserRouter>
   );
 }
